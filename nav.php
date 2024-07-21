3b33c48105355d05a0f6d59d9672ea2d510d09f9
Massimiliano <m.caggianiello@live.it>
2024-07-21 18:48:03 +0200

    modificata nav
    modificata navbar ad hamburger per schermi più piccoli

--- nav.php
+++ nav.php
@@ -1,10 +1,16 @@
 <!-- Inizio nav per tutte le pagine -->
 <nav class="divNav">
-    <ul>
-        <li class="navLink"><a href="./index.php" title="Vai a Home">HOME</a></li>
-        <li class="navLink"><a href="./chiSono.php" title="Vai a Chi Sono">CHI SONO</a></li>
-        <li class="navLink"><a href="./servizi.php" title="Vai a Servizi">SERVIZI</a></li>
-        <li class="navLink"><a href="./portfolio.php" title="Vai a Portfolio">PORTFOLIO</a></li>
+    <ul class="nav-bar">
+        <li class="menu-text">MENU</li>
+        <input type="checkbox" id="check">
+        <span class="menu">
+            <li><a href="./index.php" title="Vai a Home">HOME</a></li>
+            <li><a href="./chiSono.php" title="Vai a Chi Sono">CHI SONO</a></li>
+            <li><a href="./servizi.php" title="Vai a Servizi">SERVIZI</a></li>
+            <li><a href="./portfolio.php" title="Vai a Portfolio">PORTFOLIO</a></li>
+            <label for="check" class="close-menu"><i class="fas fa-times"></i></label>
+        </span>
+        <label for="check" class="open-menu"><i class="fas fa-bars"></i></label>
     </ul>
 </nav>
 <!-- Fine nav per tutte le pagine -->
